5ce4376216ba9d4d772797e3f9df02778f2a604a
y761350477 <761350477@qq.com>
2018-04-26 15:29:23 +0800

    1. 代码规范化. 2. 去掉lib包.

--- src/main/java/com/demo/testmybatis/MainApp.java
+++ src/main/java/com/demo/testmybatis/MainApp.java
@@ -1,6 +1,9 @@
 package com.demo.testmybatis;
 
-import org.apache.ibatis.jdbc.Null;
+
+import com.demo.testmybatis.dao.TUserMapper;
+import com.demo.testmybatis.entity.TUser;
+import com.demo.testmybatis.sqlsession.SqlSessionFactoryUtil;
 import org.apache.ibatis.session.SqlSession;
 
 import java.util.List;
@@ -10,7 +13,7 @@ public class MainApp {
     public static void main(String[] args) {
         SqlSession session = SqlSessionFactoryUtil.getSqlSession();//实例化session
         // 获取班级信息 Created by admin.
-        TUserMapper t_user = session.getMapper(com.demo.testmybatis.TUserMapper.class); // 获取封装的映射信息
+        TUserMapper t_user = session.getMapper(com.demo.testmybatis.dao.TUserMapper.class); // 获取封装的映射信息
         List<TUser> tUsers = t_user.selectByExample(null);
         for (TUser tUser : tUsers) {
             System.out.println(tUser.gettName());
